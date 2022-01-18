5d2ddf4254a1bbd37093398d8f624259e1c8b98d
Fayette Klaassen <klaassen.fayette@gmail.com>
2022-01-18 11:20:00 -0500

    sliding dates for master+knotwidth; include reinfection-only as condition

--- test-conditions.R
+++ test-conditions.R
@@ -1,7 +1,7 @@
   # master
   set0 <- data.frame(
     group               = "master",
-    dateoffset          = 0,
+    dateoffset          = 0:-40,
     "nspl_rt_knotwidth" = 5,
     "sd_omicron_delay"  = 1,
     "reinfection"       = FALSE,
@@ -12,7 +12,7 @@
   # only knotwidth
   set1 <- expand.grid(
     group               = "knotwidth",
-    dateoffset          = 0,
+    dateoffset          = 0:-40,
     "nspl_rt_knotwidth" = 10,
     "sd_omicron_delay"  = 1,
     "reinfection"       = FALSE,
@@ -25,17 +25,18 @@ nspl_rt_knotwidth = 10
 nRt = c(0, 1, 7)
 sd_omicron_delay = c(1, 10)
 reinfection = c(TRUE, FALSE)
+omicron_adjust = TRUE
 
-# omicron and reinfection tests
-set2 <- expand.grid(
-  group               = "omicron/reinfection",
-  dateoffset          = 0,
-  "nspl_rt_knotwidth" = 10,
-  "sd_omicron_delay"  = sd_omicron_delay,
-  "reinfection"       = reinfection,
-  "nRt"               = nRt, 
-  "omicron_adjust"    = TRUE
-) 
+# # omicron and reinfection tests
+# set2 <- expand.grid(
+#   group               = "omicron/reinfection",
+#   dateoffset          = 0,
+#   "nspl_rt_knotwidth" = 10,
+#   "sd_omicron_delay"  = sd_omicron_delay,
+#   "reinfection"       = reinfection,
+#   "nRt"               = nRt, 
+#   "omicron_adjust"    = onicron_adjust
+# ) 
 
 # omicron and reinfection tests, with sliding dates
 set3 <- expand.grid(
@@ -45,8 +46,19 @@ set3 <- expand.grid(
   "sd_omicron_delay"  = sd_omicron_delay,
   "reinfection"       = reinfection,
   "nRt"               = nRt, 
-  "omicron_adjust"    = TRUE
+  "omicron_adjust"    = omicron_adjust
+) 
+# omicron and reinfection tests, with sliding dates
+set4 <- expand.grid(
+  group               = "reinfection/slidingdates",
+  dateoffset          = 0:-40,
+  "nspl_rt_knotwidth" = 10,
+  "sd_omicron_delay"  = 1,
+  "reinfection"       = reinfection,
+  "nRt"               = nRt, 
+  "omicron_adjust"    = FALSE
 ) 
 
-testset <- rbind(set0, set1, set2, set3)
+testset <- rbind(set0, set1, set3, set4)
+
 
